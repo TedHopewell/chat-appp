f14f1e266cceba2e79b7dffe7dffbd3767c26c6b
TedHopewell <hopewellmaome@gmail.com>
2023-11-19 16:21:41 +0200

    latest updates to the chat app database

--- src/App.js
+++ src/App.js
@@ -3,14 +3,26 @@ import RegisterPage from "./pages/Register";
 import "./style.scss"
 import LoginPage from "./pages/Login";
 import Homepage from "./pages/Home";
+import { BrowserRouter as Router,Routes,Route } from "react-router-dom";
+
+
 
 function App() {
   return (
-    <div className="App">
-      <Homepage />
-      <RegisterPage />
-      <LoginPage />
-    </div>
+    // <div>
+    //   <RegisterPage />
+    //   <LoginPage />
+    // </div>
+    <Router>
+        <Routes>
+          <Route path='/'>
+          <Route index element={<Homepage/>}/>
+          <Route exact path='/' element={<Homepage/>}/>
+          <Route exact path='/Login' element={<LoginPage/>}/>
+          <Route exact path = '/Register' element ={<RegisterPage />} />
+          </Route>
+        </Routes>
+    </Router>
   );
 }
 
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
 import avatar from '../images/images.png'
-import { createUserWithEmailAndPassword } from 'firebase/auth';
-import { Auth } from 'firebase/auth';
+import { createUserWithEmailAndPassword, getAuth, updateProfile } from 'firebase/auth';
+import { auth, storage } from '../firebase';
+import { getStorage,
+        ref,
+        uploadBytesResumable,
+        getDownloadURL, } 
+        from 'firebase/storage';
+import { doc, setDoc} from "firebase/firestore"
+import { db } from '../firebase';
 
 const RegisterPage = () => {
   const [err,setErr] = React.useState(false);
@@ -10,10 +17,48 @@ const RegisterPage = () => {
       const displayName = e.target[0].value;
       const email = e.target[1].value;
       const password = e.target[2].value;
-      const file = e.target[3].value;
+      const file = e.target[3].files[0];
+      console.log(e.target[0].value)
+      const auth = getAuth();
+      createUserWithEmailAndPassword(auth,email,password)
+      .then((userCredentials) =>{
+        const user = userCredentials.user;
+      })
 
       try{
         const res = await createUserWithEmailAndPassword(auth,email, password);
+        const storageRef = ref(storage, displayName);
+        const uploadTask = uploadBytesResumable(storageRef, file)
+
+        
+// Register three observers:
+// 1. 'state_changed' observer, called any time the state changes
+// 2. Error observer, called on failure
+// 3. Completion observer, called on successful completion
+uploadTask.on(
+  'state_changed', 
+
+(error) => {
+  setErr(true)
+}, 
+() => {
+  // Handle successful uploads on complete
+  // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+  getDownloadURL(uploadTask.snapshot.ref).then( async(downloadURL) => {
+    await updateProfile(res.user,{
+      displayName,
+      photoURL:downloadURL,
+    });
+    await setDoc(doc(db, "users", res.user.uid),{
+      uid: res.user.uid,
+      displayName,
+      email,
+      photoURL:downloadURL,
+  });
+  });
+}
+);
+
       }catch(err){
         setErr(true);
       }
@@ -33,6 +78,7 @@ const RegisterPage = () => {
                 <span>Add an Avatar</span>
             </label>
             <button>Sign up</button>
+            {/* {err && <span>Something went wrong</span>} */}
         </form>
         <p>Already have an account? Login</p> 
       </div>
